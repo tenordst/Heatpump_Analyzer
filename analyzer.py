07e34cc7a62cf5d6206fbbf25d63d029540dcbd9
tenordst <tenordst@iki.fi>
2019-01-19 15:30:47 +0200

    Basic refactoring to object oriented done

--- analyzer.py
+++ analyzer.py
@@ -7,6 +7,7 @@ import locale
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Log csv tiedostojen indeksit
 ulkolampotila_index = 3
 lammitys_meno_index = 5
 lammitys_tulo_index = 6
@@ -22,115 +23,81 @@ kaivo_meno_raja = 2
 lisalampo_raja = -400
 mittauksia = 1500
 
-def print_summary():
+class AnalysisSummary:
+
+	def __init__(self, csvfile):
+		self.summary_writer = csv.writer(csvfile)
+		self.summary_writer.writerow(['Paivamaara'] + ['Kaynnistyksia'] + ['Pienin asteminuutti'] + ['Kaynnissa kokonaisajasta(lammitys)'] + ['Kaynnissa kokonaisajasta(vesi)'] + ['Lisalampo paalla'] + ['Keskimenolampotila'] + ['Keskitulolampotila'] + ['Keskiulkolampotila'] + ['Pienin kaivo (tulo)'] + ['Pienin kaivo (meno)'])	
+
+	def print_summary(self, log):
 		print ('*** ' + file + ' ***')
-		print ('Paivamaara: ' + pvm)
-		print ('Mittauksia: ' + str(kokonaisaika))
-		print ('Kaynnistyksia: ' + str(kaynnistykset))
-		print ('Pienin asteminuutti: ' + str(min_asteminuutti))
-		print ('Kaynnissa kokonaisajasta (lammitys): ' + str(float(kaynnissa_lammitys) / kokonaisaika * 100) + '%')
-		print ('Kaynnissa kokonaisajasta (vesi): ' + str(float(kaynnissa_vesi) / kokonaisaika * 100) + '%')
-		print ('Lisalampo paalla (min): ' + str(lisalampopaalla))
-		print ('Keskimenolampotila: ' + str(menolamposumma / kokonaisaika))
-		print ('Keskitulolampotila: ' + str(tulolamposumma / kokonaisaika))
-		print ('Pienin kaivon tulolampo: ' + str(min_kaivo_tulo))
-		print ('Pienin kaivon menolampo: ' + str(min_kaivo_meno))
-		print ('Keskiulkolampotila: ' + str(lamposumma / kokonaisaika))
+		print ('Paivamaara: ' + log.pvm)
+		print ('Mittauksia: ' + str(log.kokonaisaika))
+		print ('Kaynnistyksia: ' + str(log.kaynnistykset))
+		print ('Pienin asteminuutti: ' + str(log.min_asteminuutti))
+		print ('Kaynnissa kokonaisajasta (lammitys): ' + str(float(log.kaynnissa_lammitys) / log.kokonaisaika * 100) + '%')
+		print ('Kaynnissa kokonaisajasta (vesi): ' + str(float(log.kaynnissa_vesi) / log.kokonaisaika * 100) + '%')
+		print ('Lisalampo paalla (min): ' + str(log.lisalampopaalla))
+		print ('Keskimenolampotila: ' + str(log.menolamposumma / log.kokonaisaika))
+		print ('Keskitulolampotila: ' + str(log.tulolamposumma / log.kokonaisaika))
+		print ('Pienin kaivon tulolampo: ' + str(log.min_kaivo_tulo))
+		print ('Pienin kaivon menolampo: ' + str(log.min_kaivo_meno))
+		print ('Keskiulkolampotila: ' + str(log.lamposumma / log.kokonaisaika))
 		# print (kokonaisaika)
 
-def draw_graphs(file):
-	# plot the data
-	fig = plt.figure(num=None, figsize=(16, 10), dpi=120)
-	ax1 = fig.add_subplot(1, 1, 1, title=file, xlabel="Aika")
-	ax1.plot(t, kaivo_menolammot, 'b', t, kaivo_tulolammot, 'g',  t, tulolammot, 'y',  t, menolammot, 'c',  t, ulkolammot, 'm', t, kayttovesilammot, 'm', t, tavoitearvot, 'b')
-	ax2 = ax1.twinx()
-	ax2.plot(t,asteminuutit, 'r')
-	# ax.plot(xdata1, ydata1, 'r', xdata2, ydata2, 'b')	
-	# fig.show()
-	fig.savefig(file + ".png")
-	fig.clf()
-	plt.close(fig)
+	def add_summary_csv(self, log):
+		self.summary_writer.writerow([log.pvm,
+		str(log.kaynnistykset),
+		locale.format_string("%f",log.min_asteminuutti),
+		locale.format_string("%f",float(log.kaynnissa_lammitys) / log.kokonaisaika * 100),
+		locale.format_string("%f",float(log.kaynnissa_vesi) / log.kokonaisaika * 100),
+		str(log.lisalampopaalla),
+		locale.format_string("%f",log.menolamposumma / log.kokonaisaika),
+		locale.format_string("%f",log.tulolamposumma / log.kokonaisaika),
+		locale.format_string("%f",log.lamposumma / log.kokonaisaika),
+		locale.format_string("%f",log.min_kaivo_tulo),
+		locale.format_string("%f",log.min_kaivo_meno)])
 
-		
-def add_summary_csv():
-		summary_writer.writerow([pvm,
-		str(kaynnistykset),
-		locale.format_string("%f",min_asteminuutti),
-		locale.format_string("%f",float(kaynnissa_lammitys) / kokonaisaika * 100),
-		locale.format_string("%f",float(kaynnissa_vesi) / kokonaisaika * 100),
-		str(lisalampopaalla),
-		locale.format_string("%f",menolamposumma / kokonaisaika),
-		locale.format_string("%f",tulolamposumma / kokonaisaika),
-		locale.format_string("%f",lamposumma / kokonaisaika),
-		locale.format_string("%f",min_kaivo_tulo),
-		locale.format_string("%f",min_kaivo_meno)])
+class Log:
+	def __init__(self):
+		# Lämpösumma keskiarvojen laskemiseksi
+		self.lamposumma = 0
+		self.menolamposumma = 0
+		self.tulolamposumma = 0
+		self.kaivomenolamposumma = 0
+		self.pvm = ''	
 
-locale.setlocale(locale.LC_ALL, '')		
+		self.kaynnistykset = 0
+		self.kaynnissa_lammitys = 0
+		self.kaynnissa_vesi = 0
+		self.kokonaisaika = 0
+		self.lisalampopaalla = 0
 		
-path = './'
-if len(sys.argv) > 1:
-	path = path + sys.argv[1] + '/'
+		self.min_kaivo_meno = 1000
+		self.min_kaivo_tulo = 1000
 
-print ('Analyzing path ' + path)
+		self.min_asteminuutti = 1000
+		self.edellinen_asteminuutti = 1000
+		self.edellinen_menolampo = 1000
+		self.edellinen_kaivo_menolampo = 1000
+		self.edellinen_toimintatila = 0
+		
+		self.t = np.arange(0, mittauksia, 1)
+		self.asteminuutit = np.zeros(mittauksia) 
+		self.kaivo_menolammot = np.zeros(mittauksia)
+		self.kaivo_tulolammot = np.zeros(mittauksia)
+		self.menolammot = np.zeros(mittauksia)
+		self.tulolammot = np.zeros(mittauksia) 	
+		self.ulkolammot = np.zeros(mittauksia) 	 				
+		self.analyzeulkolammot = np.zeros(mittauksia)
+		self.kayttovesilammot = np.zeros(mittauksia)
+		self.tavoitearvot = np.zeros(mittauksia)
 
-# Get list of of all files in the current dir
-from os import walk
-f = []
-for (dirpath, dirnames, filenames) in walk(path):
-	for filename in filenames:
-		f.append(path + filename)
-	break
-	
-print (f)
-		
-with open(path + 'summary.csv', 'w', newline='') as csvfile:
-	summary_writer = csv.writer(csvfile)
-	summary_writer.writerow(['Paivamaara'] + ['Kaynnistyksia'] + ['Pienin asteminuutti'] + ['Kaynnissa kokonaisajasta(lammitys)'] + ['Kaynnissa kokonaisajasta(vesi)'] + ['Lisalampo paalla'] + ['Keskimenolampotila'] + ['Keskitulolampotila'] + ['Keskiulkolampotila'] + ['Pienin kaivo (tulo)'] + ['Pienin kaivo (meno)'])	
-	
-	for file in f:
-	  # Käydään läpi kaikki .LOG tiedostot
-		
+	def analyze(self, file):
 		if file.endswith('.LOG'):
-			# Lämpösumma keskiarvojen laskemiseksi
-			lamposumma = 0
-			menolamposumma = 0
-			tulolamposumma = 0
-			kaivomenolamposumma = 0
-			pvm = ''	
-
-			kaynnistykset = 0
-			kaynnissa_lammitys = 0
-			kaynnissa_vesi = 0
-			kokonaisaika = 0
-			lisalampopaalla = 0
-			
-			min_kaivo_meno = 1000
-			min_kaivo_tulo = 1000
-
-			edellinen_asteminuutti = 1000
-			edellinen_menolampo = 1000
-			edellinen_kaivo_menolampo = 1000
-			edellinen_toimintatila = 0
-			
-			index = 0
-			min_asteminuutti = 1000
-			pienenee = 0		
-			merkintoja = 0
-			
 			with open(file, 'r') as csvfile:
 				csvreader = csv.reader(csvfile, delimiter='	', quotechar='|')
-
-				t = np.arange(0, mittauksia, 1)
-				asteminuutit = np.zeros(mittauksia) 
-				kaivo_menolammot = np.zeros(mittauksia)
-				kaivo_tulolammot = np.zeros(mittauksia)
-				menolammot = np.zeros(mittauksia)
-				tulolammot = np.zeros(mittauksia) 		 				
-				ulkolammot = np.zeros(mittauksia)
-				kayttovesilammot = np.zeros(mittauksia)
-				tavoitearvot = np.zeros(mittauksia)
-				
-				csvreader = csv.reader(csvfile, delimiter='	', quotechar='|')
+				index = 0
 				
 				try:
 					# skip first two rows
@@ -141,66 +108,102 @@ with open(path + 'summary.csv', 'w', newline='') as csvfile:
 						# Check that row is fully written
 						if len(row) >= 20:
 							# Päivämääräksi ekan rivin päivämäärä
-							if(pvm == ''): pvm = row[0]
+							if(self.pvm == ''): self.pvm = row[0]
 							
 							# Kaivon tulolämpötila 
 							kaivo_tulolampo = float(row[kaivo_tulo_index ]) / 10
-							if (min_kaivo_tulo > kaivo_tulolampo): min_kaivo_tulo = kaivo_tulolampo
-							# print kaivo_tulolampo
+							if (self.min_kaivo_tulo > kaivo_tulolampo): self.min_kaivo_tulo = kaivo_tulolampo
 							
 							# Kaivon menolämpötila
 							kaivo_menolampo = float(row[kaivo_meno_index ]) / 10
-							if (min_kaivo_meno > kaivo_menolampo): min_kaivo_meno = kaivo_menolampo
-							# print kaivo_menolampo
+							if (self.min_kaivo_meno > kaivo_menolampo): self.min_kaivo_meno = kaivo_menolampo
 							
 							# Lasketaan kaynnissa oloajat + käynnistysten määrä
 							toimintatila = float(row[toimintatila_index])
 							# print toimintatila
-							if (toimintatila == 30): kaynnissa_lammitys = kaynnissa_lammitys + 1
-							if (toimintatila == 20): kaynnissa_vesi = kaynnissa_vesi + 1
-							if (edellinen_toimintatila == 10 and toimintatila > 10): kaynnistykset = kaynnistykset + 1
+							if (toimintatila == 30): self.kaynnissa_lammitys = self.kaynnissa_lammitys + 1
+							if (toimintatila == 20): self.kaynnissa_vesi = self.kaynnissa_vesi + 1
+							if (self.edellinen_toimintatila == 10 and toimintatila > 10): 
+								self.kaynnistykset = self.kaynnistykset + 1
 							
 							# Asteminuutti ja onko lisälämpöpäällä
 							asteminuutti = float(row[asteminuutit_index]) / 10
 							# print asteminuutti
-							if (min_asteminuutti > asteminuutti): min_asteminuutti = asteminuutti
-							if (asteminuutti < lisalampo_raja): lisalampopaalla = lisalampopaalla + 1
+							if (self.min_asteminuutti > asteminuutti): self.min_asteminuutti = asteminuutti
+							if (asteminuutti < lisalampo_raja): self.lisalampopaalla = self.lisalampopaalla + 1
 							
 							# Lasketaan lämposummat
-							lamposumma = lamposumma + float(row[ulkolampotila_index]) / 10
-							menolamposumma = menolamposumma + float(row[lammitys_meno_index]) / 10
-							tulolamposumma = tulolamposumma + float(row[lammitys_tulo_index]) / 10
+							self.lamposumma = self.lamposumma + float(row[ulkolampotila_index]) / 10
+							self.menolamposumma = self.menolamposumma + float(row[lammitys_meno_index]) / 10
+							self.tulolamposumma = self.tulolamposumma + float(row[lammitys_tulo_index]) / 10
 							
 							# Talletetaan muutama arvo seuraava kierrosta varten (vertailu)
-							edellinen_asteminuutti = asteminuutti
-							edellinen_toimintatila = toimintatila
+							self.edellinen_asteminuutti = asteminuutti
+							self.edellinen_toimintatila = toimintatila
 							
-							asteminuutit[index] = asteminuutti
-							kaivo_menolammot[index] = kaivo_menolampo
-							kaivo_tulolammot[index] = kaivo_tulolampo
-							menolammot[index] = float(row[lammitys_meno_index]) / 10
-							tulolammot[index] = float(row[lammitys_tulo_index]) / 10
-							ulkolammot[index] = float(row[ulkolampotila_index]) / 10
-							kayttovesilammot[index] = float(row[kayttovesi_index]) / 10
-							tavoitearvot[index] = float(row[laskettu_meno_index]) / 10
+							self.asteminuutit[index] = asteminuutti
+							self.kaivo_menolammot[index] = kaivo_menolampo
+							self.kaivo_tulolammot[index] = kaivo_tulolampo
+							self.menolammot[index] = float(row[lammitys_meno_index]) / 10
+							self.tulolammot[index] = float(row[lammitys_tulo_index]) / 10
+							self.ulkolammot[index] = float(row[ulkolampotila_index]) / 10
+							self.kayttovesilammot[index] = float(row[kayttovesi_index]) / 10
+							self.tavoitearvot[index] = float(row[laskettu_meno_index]) / 10
 							index = index + 1
 				
 					# Login pituus/kokonaisaika on rivien määrä - 2
-					kokonaisaika = csvreader.line_num - 2		
+					self.kokonaisaika = csvreader.line_num - 2		
 
-					# Luo yhteenvedot vain logeista jossa vähintään 10 tuntia logia (1 entry per minuutti)
-					if (kokonaisaika > 600): 
-						# Tulostetaan yhteenveto
-						print_summary()
-						
-						# Talletaan yhteenveto tiedostoon
-						add_summary_csv()
-					
-					#Piirretään kuvaaja
-					draw_graphs(file)
 				except csv.Error as e:
 					print ("Parsing error " + e + " + with file " + file)
 			
+
+	def draw_graphs(self, file):
+		# plot the data
+		fig = plt.figure(num=None, figsize=(16, 10), dpi=120)
+		ax1 = fig.add_subplot(1, 1, 1, title=file, xlabel="Aika")
+		ax1.plot(self.t, self.kaivo_menolammot, 'b',self.t, self.kaivo_tulolammot, 'g',  self.t, self.tulolammot, 'y',  self.t, self.menolammot, 'c', self.t, self.ulkolammot, 'm', self.t, self.kayttovesilammot, 'm', self.t, self.tavoitearvot, 'b')
+		ax2 = ax1.twinx()
+		ax2.plot(self.t,self.asteminuutit, 'r')
+		# ax.plot(xdata1, ydata1, 'r', xdata2, ydata2, 'b')	
+		# fig.show()
+		fig.savefig(file + ".png")
+		fig.clf()
+		plt.close(fig)
+
+# Pääfunktio
+locale.setlocale(locale.LC_ALL, '')		
+		
+path = './'
+if len(sys.argv) > 1:
+	path = path + sys.argv[1] + '/'
+
+# Get list of of all files in the current dir
+from os import walk
+f = []
+for (dirpath, dirnames, filenames) in walk(path):
+	for filename in filenames:
+		f.append(path + filename)
+	break
+		
+with open(path + 'summary.csv', 'w', newline='') as csvfile:	
+	summary = AnalysisSummary(csvfile)
+	for file in f:
+		log = Log()
+		# Analysoidaan kaikki .LOG tiedostot
+		log.analyze(file)
+		# Luo yhteenvedot vain logeista jossa vähintään 10 tuntia logia (1 entry per minuutti)
+		if (log.kokonaisaika > 600): 
+			# Tulostetaan yhteenveto
+			summary.print_summary(log)
+			
+			# Talletaan yhteenveto tiedostoon
+			summary.add_summary_csv(log)
+		
+		# Piirretään kuvaaja
+		log.draw_graphs(file)
+		
+		
 
 		
 
